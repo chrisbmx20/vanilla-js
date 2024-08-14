f9b42bde639f90fa5910a903b99e4c439a318995
chrisbmx20 <calvarez@fwdcostarica.com>
2024-08-14 10:44:49 -0600

    proyecto node

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,27 @@
-// Inserte el código aquí
+function clonarArreglo(array){
+    return [...array]
+}
+
+function clonarObjeto(obj){
+
+    return obj 
+}
+
+let numeros = [1,2,3]
+function sumar(nums){
+  return  nums.reduce((acc, currentValue) => acc+currentValue)
+}
+
+function saludar(nombre){
+return `hola ${nombre}`
+}
+console.log(sumar(numeros));
+
+console.log(saludar("desconocido"));
+
+
 
 
 
 // NO MODIFICAR
-export { clonarArreglo, clonarObjeto, sumar, saludar };
+//export { clonarArreglo, clonarObjeto, sumar, saludar };
