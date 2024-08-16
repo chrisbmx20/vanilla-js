d5db361f6b3d188cf8277ee7406d18b0552d7a4a
chrisbmx20 <calvarez@fwdcostarica.com>
2024-08-16 08:46:09 -0600

    commit

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -8,9 +8,15 @@ function clonarObjeto(obj){
 }
 
 let numeros = [1,2,3]
+
 function sumar(nums){
-  return  nums.reduce((acc, currentValue) => acc+currentValue)
+  return  nums.reduce((acc, item) => acc + item)
 }
+
+numeros.forEach((item,indice) =>{
+    console.log(item);
+    console.log(indice);
+})
 
 function saludar(nombre){
 return `hola ${nombre}`
@@ -18,6 +24,14 @@ return `hola ${nombre}`
 console.log(sumar(numeros));
 
 console.log(saludar("desconocido"));
+
+console.log(clonarArreglo(numeros));
+
+console.log(clonarObjeto({uno:1}));
+
+
+
+
 
 
 
